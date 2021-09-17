a1ae0cca121775d57ce7c91a6578dd30f501d0d2
PaulaDC2725 <pcdelgado3@misena.edu.co>
2021-09-16 20:58:21 -0500

    Menu editado

--- src/MainMenu.jsx
+++ src/MainMenu.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const MainMenu = () => (
-    <ul>
-        <li><NavLink exact to="/">Home</NavLink></li>
-        <li><NavLink exact to="/personajes">Personajes</NavLink></li>
-        <li><NavLink exact to="/contacto">Contacto</NavLink></li>
-    </ul>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+            <center>
+                <li class="nav-item active"><NavLink exact to="/">Home</NavLink></li>
+                <li class="nav-item active"><NavLink exact to="/personajes">Personajes</NavLink></li>
+                <li class="nav-item active"><NavLink exact to="/contacto">Contacto</NavLink></li>
+            </center>
+            </ul>
+        </div>    
+    </nav>
+    
 )
 
 export default MainMenu;
